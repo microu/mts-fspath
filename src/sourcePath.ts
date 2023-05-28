05a63d23dd9894567ab0d5736984e886a4ac4350
michelr <michel.rousset@free.fr>
2023-05-28 16:28:37 +0200

    ...

--- src/sourcePath.ts
+++ src/sourcePath.ts
@@ -1,12 +1,27 @@
+import path from "node:path";
 import { FSPath, FSPathArg, fsPath } from "./fsPath";
 
 export class SourcePath extends FSPath {
   readonly localPath: FSPath;
   readonly basePath: FSPath;
 
-  constructor(basePath: FSPathArg, localPath: FSPathArg) {
-    localPath = fsPath(localPath);
-    basePath = fsPath(basePath);
+  constructor(path: FSPathArg);
+  constructor(basePath: FSPathArg, localPath: FSPathArg);
+  constructor(pathOrBase: FSPathArg, localPath?: FSPathArg) {
+    pathOrBase = fsPath(pathOrBase);
+    let basePath: FSPath;
+    if (localPath == undefined) {
+      if (pathOrBase.isAbsolute()) {
+        localPath = new FSPath(path.relative("/", pathOrBase.path));
+        basePath = new FSPath("/");
+      } else {
+        localPath = pathOrBase;
+        basePath = new FSPath(".");
+      }
+    } else {
+      localPath = fsPath(localPath);
+      basePath = pathOrBase;
+    }
 
     if (localPath.isAbsolute()) {
       throw new Error(`localPath (${localPath}) must be relative`);
@@ -23,4 +38,33 @@ export class SourcePath extends FSPath {
   withLocalPath(localPath: FSPathArg): SourcePath {
     return new SourcePath(this.base, localPath);
   }
+}
+
+type SourceDest = {
+  source: SourcePath;
+  dest: SourcePath;
+};
+
+export type SourceDestMapperFunc = (bp: SourcePath) => SourcePath;
+export type SourceDestMapper = SourceDestMapperFunc | SourceDestMapperFunc[];
+
+export function iterSourceDest(
+  sources: Iterable<SourcePath>,
+  mapper: SourceDestMapper
+): Iterable<SourceDest> {
+  const mapSrcToDst = function (sp: SourcePath): SourcePath {
+    const mappers = mapper instanceof Array ? mapper : [mapper];
+    for (const m of mappers) {
+      sp = m(sp);
+    }
+    return sp;
+  };
+
+  return {
+    *[Symbol.iterator]() {
+      for (const source of sources) {
+        yield { source: source, dest: mapSrcToDst(source) };
+      }
+    },
+  };
 }
--- test/sourcePath.test.ts
+++ test/sourcePath.test.ts
@@ -1,6 +1,6 @@
 import { describe, test } from "node:test";
 import assert from "node:assert/strict";
-import { SourcePath } from "@src/sourcePath";
+import { SourcePath, iterSourceDest } from "@src/sourcePath";
 
 describe("SourcePath class", () => {
   test("Basic Usage", () => {
@@ -17,5 +17,56 @@ describe("SourcePath class", () => {
     assert.throws(() => {
       new SourcePath("/home", "/home/michel/.bashrc");
     }, "relative");
+  });
+
+  test("One arg constructor", () => {
+    const spa = new SourcePath("/home/michel/www/index.html");
+    assert.equal(spa.localPath.path, "home/michel/www/index.html");
+    assert.equal(spa.basePath.path, "/");
+    assert.equal(spa.path, "/home/michel/www/index.html");
+
+    const spb = new SourcePath("docs/index.html");
+    assert.equal(spb.localPath.path, "docs/index.html");
+    assert.equal(spb.basePath.path, ".");
+    assert.equal(spb.path, "docs/index.html");
+  });
+});
+
+describe("iterSourceDest", () => {
+  test("Basic usage", () => {
+    const bpm = iterSourceDest(
+      [new SourcePath("/home", "a.md"), new SourcePath("/home", "b.md")],
+      (sp: SourcePath) => new SourcePath("out", sp.localPath)
+    );
+    const mappings = [...bpm];
+    assert.equal(mappings.length, 2);
+    assert.deepEqual(
+      mappings.map((sd) => sd.source.path),
+      ["/home/a.md", "/home/b.md"]
+    );
+    assert.deepEqual(
+      mappings.map((sd) => sd.dest.path),
+      ["out/a.md", "out/b.md"]
+    );
+  });
+
+  test("Multiple mapper", () => {
+    const bpm = iterSourceDest(
+      [new SourcePath("/home", "a.md"), new SourcePath("/home", "b.md")],
+      [
+        (sp: SourcePath) => new SourcePath("out", sp.localPath),
+        (sp: SourcePath) => new SourcePath(sp.basePath, sp.localPath + ".dump"),
+      ]
+    );
+    const mappings = [...bpm];
+    assert.equal(mappings.length, 2);
+    assert.deepEqual(
+      mappings.map((ds) => ds.source.path),
+      ["/home/a.md", "/home/b.md"]
+    );
+    assert.deepEqual(
+      mappings.map((ds) => ds.dest.path),
+      ["out/a.md.dump", "out/b.md.dump"]
+    );
   });
 });
